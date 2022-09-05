8ce9b235c5b24c0640ae21bfc60a667331e4094f
michalzolyniak <michalzolyniak@o2.pl>
2022-09-05 16:17:55 +0200

    start

---
+++ fridge/fridge_management/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+from django.conf import settings
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    consumption_date_close = models.DateField()
+    consumption_hours = models.IntegerField()
+    category = models.ManyToManyField(Category)
+    default_price = models.DecimalField(max_digits=5, decimal_places=2)
+
+
+class Fridge(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    product = models.ForeignKey(Product)
+    purchase_price = models.DecimalField(max_digits=5, decimal_places=2)
+    date_added = models.DateTimeField()
+
+
+class Note(models.Model):
+    product = models.ForeignKey(Product)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    notes = models.TextField()
