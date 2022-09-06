8f9743463a27b86392a64e48fd0ea95b01f657df
michalzolyniak <michalzolyniak@o2.pl>
2022-09-06 22:56:11 +0200

    login ready

--- fridge/fridge/urls.py
+++ fridge/fridge/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from fridge_management.views import UserCreateView, LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('add_user/', UserCreateView.as_view(), name='add-user'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('fridge/', LogoutView.as_view(), name='fridge'),
 ]
--- fridge/fridge_management/views.py
+++ fridge/fridge_management/views.py
@@ -1,3 +1,47 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model, login, \
+    authenticate, logout
+from django.contrib.auth.mixins import PermissionRequiredMixin
+from django.views.generic import FormView, ListView, RedirectView
+from django.views.generic import FormView, CreateView, UpdateView
+from django.urls import reverse_lazy
+from .forms import UserCreateForm, LoginForm
 
-# Create your views here.
+User = get_user_model()
+
+
+class UserCreateView(FormView):
+    template_name = 'fridge/user_create.html'
+    form_class = UserCreateForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        cd = form.cleaned_data
+        User.objects.create_user(
+            username=cd['login'],
+            password=cd['password1'],
+            email=cd['email'],
+        )
+        return response
+
+
+class LoginView(FormView):
+    template_name = 'fridge/login.html'
+    form_class = LoginForm
+    success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        cd = form.cleaned_data
+        user = authenticate(username=cd['login'], password=cd['password'])
+        login(self.request, user)
+        return response
+
+
+class LogoutView(RedirectView):
+    url = reverse_lazy('login')
+
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return super().get(request, *args, **kwargs)
